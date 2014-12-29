01e90461bf9a6ed62823fc6b6d8569ce3d71dea0
Jacob Lowe <jacoblowe2.0@gmail.com>
2014-12-28 21:23:24 -0800

    omar revisions

--- public/js/main.js
+++ public/js/main.js
@@ -1,26 +1,36 @@
 
 
 var $ = window.jQuery = require( 'jquery' ),
+    merge = require( 'merge' ),
     flickr = require( './flickrphotos' ),
-    hero = require( './components/hero' ),
-    fancyboxOptions = {
-        openEffect  : 'none',
-        closeEffect : 'none',
-        showNavArrows: false,
-        helpers : {
-            media : {}
-        }
-    };
+    hero = require( './components/hero' );
 
 // monkey patch jquery plugins
 require( './jquery.fancybox.pack.js' );
 require( './jquery.fancybox-media.js' );
 
 $(function() {
+    var $body = $( 'body' ),
+        fancyboxOptions = {
+            openEffect  : 'none',
+            closeEffect : 'none',
+            beforeShow: function(){
+                $body.css({ overflowY: 'hidden' })
+            },
+            afterClose: function(){
+                $body.css({ overflowY: 'visible' })
+            },
+            helpers : {
+                media : {}
+            }
+        };
+
     
     hero();
     flickr.addPhotos(); // this starts the script
-    $( 'a[rel=lightbox-video]' ).fancybox( fancyboxOptions );
-    $('a[rel=lightbox]').fancybox( );
+    $( 'a[rel=lightbox-video]' ).fancybox( merge( {}, fancyboxOptions, { 
+        showNavArrows: false 
+    } ) );
+    $('a[rel=lightbox]').fancybox( fancyboxOptions );
 
 });
